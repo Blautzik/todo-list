3f5ed3fee889e790cc2ed4bd0e1fe6a7f3e072d1
federico blautzik <fedeblau@gmail.com>
2023-03-04 21:24:23 -0300

    entrega 3
    .

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
-import LandingSlider from '../components/LandingSlider';
+
 import OnboardingSlider from '../components/OnboardingSlider';
 
 const Login = () => {
@@ -15,7 +15,6 @@ const Login = () => {
 
     return (
         <View className='items-center justify-center h-screen'>
-            {/*/<LandingSlider  navigation={navigation}/>*/}
             <OnboardingSlider navigation={navigation}/>
         </View>
     )
